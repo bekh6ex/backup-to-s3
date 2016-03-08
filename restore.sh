33404e8f1154186ec4ccf08c302649fdec3e2436
bekh <6ex@mail.ru>
2016-03-08 22:07:37 +0300

    fix unexistent schema

--- restore.sh
+++ restore.sh
@@ -4,7 +4,8 @@ s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET
 
 for DIR in /backup/db/*; do
   DB=${DIR##*/}
-  for FILE in ${DIR}; do
+  echo "CREATE SCHEMA IF NOT EXISTS ${DB};" | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS}
+  for FILE in ${DIR}/*.gz; do
     echo -n Restoring ${FILE} ...
     gzip -d -c ${FILE} | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} ${DB}
     echo ' DONE'
