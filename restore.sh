416f669def3b4729479276b27cb47c55694a81ba
bekh <6ex@mail.ru>
2016-03-08 20:36:22 +0300

    fix recovery

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET_KEY} s3://${S3_BUCKET}/ /backup
+s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET_KEY} s3://${S3_BUCKET}/ /backup/
 
 for DIR in /backup/db/*; do
   DB=${DIR##*/}
