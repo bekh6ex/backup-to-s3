de4d178662c684466c23ecdb2d5de31e2f5ab1a2
bekh <6ex@mail.ru>
2016-03-19 00:28:45 +0300

    Restoring separately

--- restore.sh
+++ restore.sh
@@ -23,22 +23,51 @@ function ask_yn {
     fi
 }
 
+RESTORE_DB=0
+RESTORE_FILES=0
+
+for action in "$@"
+do
+    case "$action" in
+    "db" )
+      RESTORE_DB=1
+      ;;
+    "files" )
+      RESTORE_FILES=1
+      ;;
+    * )
+      echo "Unknown argument: ${action}"
+      exit 1;
+    esac
+done
+
+
 if ask_yn "Do yoy want to restore data from backup?";
 then
-    s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET_KEY} s3://${S3_BUCKET}/ /backup/
+    if [[ $RESTORE_DB == 1 ]]
+    then
+        echo "Restoring database"
+        s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET_KEY} s3://${S3_BUCKET}/db /backup/
 
-    for DIR in /backup/db/*; do
-      DB=${DIR##*/}
-      echo "CREATE SCHEMA IF NOT EXISTS ${DB};" | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS}
-      for FILE in ${DIR}/*.gz; do
-        echo -n Restoring ${FILE} ...
-        gzip -d -c ${FILE} | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} ${DB}
-        echo ' DONE'
-      done
-    done
+        for DIR in /backup/db/*; do
+          DB=${DIR##*/}
+          echo "CREATE SCHEMA IF NOT EXISTS ${DB};" | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS}
+          for FILE in ${DIR}/*.gz; do
+            echo -n Restoring ${FILE} ...
+            gzip -d -c ${FILE} | mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASS} ${DB}
+            echo ' DONE'
+          done
+        done
+    fi
+
+    if [[ $RESTORE_FILES == 1 ]]
+    then
+        echo "Restoring files"
+        s3cmd sync --no-check-md5 --access_key=${S3_ACCESS_KEY} --secret_key=${S3_SECRET_KEY} s3://${S3_BUCKET}/files /backup/
+    fi
 
     exit 0
 else
     echo "Canceled!"
-    exit 1
+    exit 255
 fi
